e24b4f06ef71b130ca46123a79872396c1258b5e
CyberRor <87041881+Rordan1@users.noreply.github.com>
2024-05-16 16:22:55 +0000

    updated

--- IAM/.sh
+++ IAM/.sh
@@ -1,6 +1,38 @@
-// set up users and groups
 
-aws 
+# make admin account
 
-// permissons policies
+# new user "Admin"
+
+# attach AdministratorAccess policy
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": "*",
+#             "Resource": "*"
+#         }
+#     ]
+# }
+
+#  set up users + check if successful
+
+
+aws iam create-user --user-name Carlos
+aws iam create-user --user-name Novak
+aws iam create-user --user-name Roger
+
+aws iam list-users
+
+
+#  set up groups + check if successful
+
+aws iam add-user-to-group --group-name Developer --user-name Carlos
+aws iam add-user-to-group --group-name Engineers --user-name Novak --user-name Roger
+
+aws iam list-groups
+
+
+# permissons policies 
 
