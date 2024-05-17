70f230d02acf8ebc8e3a5688688c5190847d871d
CyberRor <87041881+Rordan1@users.noreply.github.com>
2024-05-17 16:31:01 +0000

    policies

--- IAM/.sh
+++ IAM/.sh
@@ -41,8 +41,22 @@ aws iam add-user-to-group --group-name Engineers --user-name Novak
 
 # permissons policies 
 
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSFullAccess \
+ --group-name Developer
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSDataFullAccess \
+ --group-name Developer
+
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess \
  --group-name Engineers
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess \
+ --group-name Engineers
 
+# access keys 
 
+aws iam create-access-key \
+    --user-name Carlos
+aws iam create-access-key \
+    --user-name Roger
+aws iam create-access-key \
+    --user-name Novak
 
